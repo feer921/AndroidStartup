d943eb0ed1a314aa761643c3cfefbc14189b83cb
fee <feer921@163.com>
2023-03-11 20:25:39 +0800

    feat: 更新 gradle 版本

--- theAndroidStartup/src/main/java/com/github/androidstartup/AStartupTask.kt
+++ theAndroidStartup/src/main/java/com/github/androidstartup/AStartupTask.kt
@@ -12,14 +12,14 @@ package com.github.androidstartup
  */
 abstract class AStartupTask<T>: IStartupTask<T> {
     /**
-     * 本启动任务所信赖的其他 task的数量
+     * 本启动任务所依赖的其他 task的数量
      */
     override var dependenceTaskCount: Int
         get() = dependentTask()?.size ?: 0
         set(value) {}
 
     /**
-     * 本启动任务所信赖的其他启动任务的 Class数据集
+     * 本启动任务所依赖的其他启动任务的 Class数据集
      * 作用为：在本启动任务执行前，需要在所依赖其他任务执行完后再执行
      */
     override fun dependentTask(): List<Class<in IStartupTask<*>>>? {
--- theAndroidStartup/src/main/java/com/github/androidstartup/IStartupTask.kt
+++ theAndroidStartup/src/main/java/com/github/androidstartup/IStartupTask.kt
@@ -21,13 +21,13 @@ interface IStartupTask<T> {
     fun doStartupTask(context: Context): T
 
     /**
-     * 本启动任务所信赖的其他 task的数量
+     * 本启动任务所依赖的其他 task的数量
      */
     var dependenceTaskCount: Int
 
 
     /**
-     * 本启动任务所信赖的其他启动任务的 Class数据集
+     * 本启动任务所依赖的其他启动任务的 Class数据集
      * 作用为：在本启动任务执行前，需要在所依赖其他任务执行完后再执行
      */
     fun dependentTask(): List<Class<in IStartupTask<*>>>?
